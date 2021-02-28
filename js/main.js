69866d50b3c2dad173e94c386efc2afaeb5fafca
omar mokhtar <o.attia@nhc.sa>
2021-02-28 10:38:02 +0300

    css

--- js/main.js
+++ js/main.js
@@ -23,6 +23,32 @@ slidesToShow: 3 ,
 slidesToScroll:3,
 autoplay:true,
 rtl:true,
+responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 890,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+		slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+
+  ]
 
 });
 
@@ -65,6 +91,33 @@ slidesToShow: 3 ,
 slidesToScroll:1,
 autoplay:true,
 rtl:true,
+responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 890,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+
+  ]
+
 
 });
 
